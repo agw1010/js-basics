0fe2f58e4f68c1920afd2ea5a7c6cb6d8d398201
agw1010 <42842700+agw1010@users.noreply.github.com>
2019-03-28 00:57:31 +0900

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,6 +1,18 @@
-const API_KEY = "2789b8fc16135f1442c1eb41cb3dfe00"
-
+const weather = document.querySelector(".js-weather");
+const API_KEY = "2789b8fc16135f1442c1eb41cb3dfe00";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function (response) {
+        return response.json();
+    })
+        .then(function (json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`
+        });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +26,7 @@ function handleGeoSocces(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -29,7 +42,8 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 function init() {
